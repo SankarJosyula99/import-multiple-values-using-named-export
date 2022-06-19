02c846794f0ae6a31b049f0af4d96f3f8b0cc09b
sankar <sankar@example.com>
2022-06-19 13:00:46 +0000

    import multiple values using named export solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+let myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+let multiplyByFour = (value) => {
+  return value * 4;
+};
+export { myArray, bulb, multiplyByFour };
